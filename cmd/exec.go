8fd0d9774b37949a3cd5e1eb5fc8f063ddf6969a
vcaesar <vzvway@gmail.com>
2017-12-21 22:37:50 +0800

    add exec cmd func

---
+++ cmd/exec.go
@@ -0,0 +1,69 @@
+// Copyright 2016 The go-vgo Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// https://github.com/go-ego/ego/blob/master/LICENSE
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+package cmd
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os/exec"
+)
+
+// ExecSh exec command shell
+func ExecSh(str string, args ...string) (string, error) {
+	var (
+		cmdName = "/bin/bash"
+		params  = "-c"
+	)
+
+	if len(args) > 0 {
+		cmdName = args[0]
+	}
+
+	if len(args) > 1 {
+		params = args[1]
+	}
+
+	cmd := exec.Command(cmdName, params, str)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+	return out.String(), err
+}
+
+// ExecCmd exex command
+func ExecCmd(cmdName string, params []string) bool {
+	cmd := exec.Command(cmdName, params...)
+
+	stdout, err := cmd.StdoutPipe()
+
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	cmd.Start()
+
+	reader := bufio.NewReader(stdout)
+	for {
+		line, err2 := reader.ReadString('\n')
+		if err2 != nil || io.EOF == err2 {
+			break
+		}
+		fmt.Println(line)
+	}
+
+	cmd.Wait()
+	return true
+}
