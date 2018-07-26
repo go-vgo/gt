f22b0d39c33d999bd3397d3250f4dba0c5b7aeb3
vcaesar <vzvway@gmail.com>
2018-07-26 18:23:00 +0800

    semester file func

--- file/file.go
+++ file/file.go
@@ -13,196 +13,16 @@ package file
 // package gt
 
 import (
-	"errors"
 	"fmt"
-	"hash"
 	"io"
 	"log"
 	"os"
 	"path"
 	"strings"
 
-	"crypto/sha1"
-	"crypto/sha256"
 	"io/ioutil"
 	"path/filepath"
 )
-
-// Exist checks whether a file or directory exists.
-// It returns false when the file or directory does not exist.
-func Exist(fileName string) bool {
-	_, err := os.Stat(fileName)
-	return err == nil || os.IsExist(err)
-}
-
-// PathExists path is exist
-func PathExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
-}
-
-// Is returns true if path is a file,
-// or returns false when it's a directory or not exist.
-func Is(filePath string) bool {
-	f, err := os.Stat(filePath)
-	if err != nil {
-		return false
-	}
-	return !f.IsDir()
-}
-
-// Search Search a file in paths.
-func Search(fileName string, paths ...string) (fullpath string, err error) {
-	for _, path := range paths {
-		if fullpath = filepath.Join(path, fileName); Exist(fullpath) {
-			return
-		}
-	}
-	err = errors.New(fullpath + " not found in paths")
-	return
-}
-
-// Size returns file size in bytes and possible error.
-func Size(file string) (int64, error) {
-	f, err := os.Stat(file)
-	if err != nil {
-		return 0, err
-	}
-	return f.Size(), nil
-}
-
-// MTime returns file modified time and possible error.
-func MTime(file string) (int64, error) {
-	f, err := os.Stat(file)
-	if err != nil {
-		return 0, err
-	}
-	return f.ModTime().Unix(), nil
-}
-
-// Sha open file return sha
-func Sha(filePath string, args ...string) (sha string, err error) {
-	file, fsErr := os.Open(filePath)
-	if fsErr != nil {
-		return "", fsErr
-	}
-	defer file.Close()
-
-	if len(args) > 0 {
-		sha, err = IoSha(file, args[0])
-		return
-	}
-
-	sha, err = IoSha(file)
-	return
-}
-
-// IoSha file sha
-func IoSha(fileIO *os.File, args ...string) (string, error) {
-	var h hash.Hash
-
-	if len(args) > 0 {
-		h = sha256.New()
-	} else {
-		h = sha1.New()
-	}
-
-	_, err := io.Copy(h, fileIO)
-	if err != nil {
-		return "", err
-	}
-
-	sha := fmt.Sprintf("%x", h.Sum(nil))
-
-	return sha, nil
-}
-
-// Copy copies file from source to target path.
-func Copy(src, dst string) error {
-	// Gather file information to set back later.
-	si, err := os.Lstat(src)
-	if err != nil {
-		return err
-	}
-
-	// Handle symbolic link.
-	if si.Mode()&os.ModeSymlink != 0 {
-		target, err := os.Readlink(src)
-		if err != nil {
-			return err
-		}
-		// NOTE: os.Chmod and os.Chtimes don't recoganize symbolic link,
-		// which will lead "no such file or directory" error.
-		return os.Symlink(target, dst)
-	}
-
-	sr, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer sr.Close()
-
-	dw, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer dw.Close()
-
-	if _, err = io.Copy(dw, sr); err != nil {
-		return err
-	}
-
-	// Set back file information.
-	if err = os.Chtimes(dst, si.ModTime(), si.ModTime()); err != nil {
-		return err
-	}
-	return os.Chmod(dst, si.Mode())
-}
-
-// CopyFile copies file from source to target path.
-func CopyFile(src, dst string) (int64, error) {
-	srcFile, err := os.Open(src)
-	if err != nil {
-		return 0, err
-	}
-	defer srcFile.Close()
-
-	// if Exist(dst) != true {
-	if !Exist(dst) {
-		Write("", dst)
-	}
-
-	dstFile, err := os.Create(dst)
-	if err != nil {
-		// fmt.Println(err.Error())
-		return 0, err
-	}
-	defer dstFile.Close()
-	return io.Copy(dstFile, srcFile)
-}
-
-// OpenCopy open and copy file
-func OpenCopy(srcName, dstName string) (int64, error) {
-	src, err := os.Open(srcName)
-	if err != nil {
-		return 0, err
-	}
-	defer src.Close()
-
-	dst, err := os.OpenFile(dstName, os.O_WRONLY|os.O_CREATE, 0644)
-	if err != nil {
-		fmt.Println(err)
-		return 0, err
-	}
-	defer dst.Close()
-	return io.Copy(dst, src)
-}
 
 // Read read file and return string
 func Read(fileName string) (string, error) {
@@ -343,4 +163,85 @@ func WalkDir(dirPth, suffix string) (files []string, err error) {
 			return nil
 		})
 	return files, err
+}
+
+// Copy copies file from source to target path.
+func Copy(src, dst string) error {
+	// Gather file information to set back later.
+	si, err := os.Lstat(src)
+	if err != nil {
+		return err
+	}
+
+	// Handle symbolic link.
+	if si.Mode()&os.ModeSymlink != 0 {
+		target, err := os.Readlink(src)
+		if err != nil {
+			return err
+		}
+		// NOTE: os.Chmod and os.Chtimes don't recoganize symbolic link,
+		// which will lead "no such file or directory" error.
+		return os.Symlink(target, dst)
+	}
+
+	sr, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sr.Close()
+
+	dw, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dw.Close()
+
+	if _, err = io.Copy(dw, sr); err != nil {
+		return err
+	}
+
+	// Set back file information.
+	if err = os.Chtimes(dst, si.ModTime(), si.ModTime()); err != nil {
+		return err
+	}
+	return os.Chmod(dst, si.Mode())
+}
+
+// CopyFile copies file from source to target path.
+func CopyFile(src, dst string) (int64, error) {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer srcFile.Close()
+
+	// if Exist(dst) != true {
+	if !Exist(dst) {
+		Write("", dst)
+	}
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		// fmt.Println(err.Error())
+		return 0, err
+	}
+	defer dstFile.Close()
+	return io.Copy(dstFile, srcFile)
+}
+
+// OpenCopy open and copy file
+func OpenCopy(srcName, dstName string) (int64, error) {
+	src, err := os.Open(srcName)
+	if err != nil {
+		return 0, err
+	}
+	defer src.Close()
+
+	dst, err := os.OpenFile(dstName, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	defer dst.Close()
+	return io.Copy(dst, src)
 }
---
+++ file/sys.go
@@ -0,0 +1,108 @@
+package file
+
+import (
+	"errors"
+	"fmt"
+	"hash"
+	"io"
+	"os"
+
+	"crypto/sha1"
+	"crypto/sha256"
+	"path/filepath"
+)
+
+// Exist checks whether a file or directory exists.
+// It returns false when the file or directory does not exist.
+func Exist(fileName string) bool {
+	_, err := os.Stat(fileName)
+	return err == nil || os.IsExist(err)
+}
+
+// PathExists path is exist
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+// Is returns true if path is a file,
+// or returns false when it's a directory or not exist.
+func Is(filePath string) bool {
+	f, err := os.Stat(filePath)
+	if err != nil {
+		return false
+	}
+	return !f.IsDir()
+}
+
+// Search Search a file in paths.
+func Search(fileName string, paths ...string) (fullpath string, err error) {
+	for _, path := range paths {
+		if fullpath = filepath.Join(path, fileName); Exist(fullpath) {
+			return
+		}
+	}
+	err = errors.New(fullpath + " not found in paths")
+	return
+}
+
+// Size returns file size in bytes and possible error.
+func Size(file string) (int64, error) {
+	f, err := os.Stat(file)
+	if err != nil {
+		return 0, err
+	}
+	return f.Size(), nil
+}
+
+// MTime returns file modified time and possible error.
+func MTime(file string) (int64, error) {
+	f, err := os.Stat(file)
+	if err != nil {
+		return 0, err
+	}
+	return f.ModTime().Unix(), nil
+}
+
+// Sha open file return sha
+func Sha(filePath string, args ...string) (sha string, err error) {
+	file, fsErr := os.Open(filePath)
+	if fsErr != nil {
+		return "", fsErr
+	}
+	defer file.Close()
+
+	if len(args) > 0 {
+		sha, err = IoSha(file, args[0])
+		return
+	}
+
+	sha, err = IoSha(file)
+	return
+}
+
+// IoSha file sha
+func IoSha(fileIO *os.File, args ...string) (string, error) {
+	var h hash.Hash
+
+	if len(args) > 0 {
+		h = sha256.New()
+	} else {
+		h = sha1.New()
+	}
+
+	_, err := io.Copy(h, fileIO)
+	if err != nil {
+		return "", err
+	}
+
+	sha := fmt.Sprintf("%x", h.Sum(nil))
+
+	return sha, nil
+}
